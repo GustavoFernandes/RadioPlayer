17b0398d6519b6d33485c85d5808e32eab230b91
GustavoFernandes <GustavoFernandes@users.noreply.github.com>
2018-03-07 13:46:32 -0800

    Create main layout and add OnClickListeners. Change language to Java 8

--- app/src/main/java/com/github/gustavofernandes/radioplayer/MainActivity.java
+++ app/src/main/java/com/github/gustavofernandes/radioplayer/MainActivity.java
@@ -8,15 +8,30 @@ import android.support.v4.media.session.MediaControllerCompat;
 import android.support.v4.media.session.PlaybackStateCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private MediaBrowserCompat mMediaBrowser;
 
+    private TextView mTitleTextView;
+    private TextView mArtistTextView;
+    private Button mPreviousButton;
+    private Button mPlayPauseButton;
+    private Button mNextButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTitleTextView = findViewById(R.id.textview_title);
+        mArtistTextView = findViewById(R.id.textview_artist);
+        mPreviousButton = findViewById(R.id.button_previous);
+        mPlayPauseButton = findViewById(R.id.button_playPause);
+        mNextButton = findViewById(R.id.button_next);
 
         mMediaBrowser = new MediaBrowserCompat(
                 this,
@@ -29,6 +44,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         mMediaBrowser.connect();
+    }
+
+    private void buildTransportControls() {
+
+        // TODO: make TransportControls function-wide?
+
+        mPreviousButton.setOnClickListener(view -> MediaControllerCompat.getMediaController(MainActivity.this).getTransportControls().skipToPrevious());
+        mNextButton.setOnClickListener(view -> MediaControllerCompat.getMediaController(MainActivity.this).getTransportControls().skipToNext());
+
+        mPlayPauseButton.setOnClickListener(view -> {
+            if (MediaControllerCompat.getMediaController(MainActivity.this).getPlaybackState().getState() == PlaybackStateCompat.STATE_PLAYING) {
+                MediaControllerCompat.getMediaController(MainActivity.this).getTransportControls().pause();
+            } else {
+                MediaControllerCompat.getMediaController(MainActivity.this).getTransportControls().play();
+            }
+        });
     }
 
     private class MediaBrowserConnectionCallback extends MediaBrowserCompat.ConnectionCallback {
@@ -44,6 +75,8 @@ public class MainActivity extends AppCompatActivity {
                 mediaController.registerCallback(new MediaControllerCallback());
 
                 MediaControllerCompat.setMediaController(MainActivity.this, mediaController);
+
+                buildTransportControls();
 
             } catch (RemoteException e) {
                 // TODO
